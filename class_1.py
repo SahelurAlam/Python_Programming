ca90168f8426aee36054db8476c47c30113db7b7
unknown <>
2026-05-02 12:50:27 +0600

    class_1 successfully working

---
+++ class_1.py
@@ -0,0 +1,15 @@
+"""
+print("Name         : Ishti Ahmed")
+print("Roll         : 243533")
+print("Department   : CST")
+
+print("Batch        : 19 \nSection      : C \nInstitute    : Daffodil Polytechnic Institute",end=" Hello ji\n")
+
+print(21+25)
+print("Age : ",25)
+print("Age : ",25+52)
+print(f"Age : {20+21}")
+
+print(f"Name : Ishti Ahmed \nAge : {20}")
+"""
+
